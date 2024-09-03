2b1b7a04605beccb242460c0b89bd131743cca48
Jean Escalante <jean.r.escalante@gmail.com>
2024-09-02 22:32:00 -0400

    Added basic door interaction
    Added door prefab with a trigger volume
    Added basic interaction with door in PlayerInteraction script
    Added height reference to scene

---
+++ Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+    bool InInteractZone;
+
+    void Start()
+    {
+        InInteractZone = false;
+    }
+
+    void OnTriggerEnter(Collider InteractionVolume)
+    {
+        if (InteractionVolume.GetType() == typeof(BoxCollider))
+            InInteractZone = true;
+            Debug.Log("In Door Zone");
+    }
+
+    void OnTriggerExit(Collider InteractionVolume)
+    {
+        if (InteractionVolume.GetType() == typeof(BoxCollider))
+            InInteractZone = false;
+            Debug.Log("Left Door Zone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InInteractZone == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            Debug.Log("Open door");
+        }
+        
+    }
+}
