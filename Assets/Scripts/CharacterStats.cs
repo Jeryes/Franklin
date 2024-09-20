c64eb71d6338c79b5fbe0d13cffd6072e099e085
Jean Escalante <jean.r.escalante@gmail.com>
2024-09-19 22:29:33 -0400

    Started CharacterStats implementation on Player

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] string charName = "charName";
+    [SerializeField] int charHP = 30;
+    [SerializeField] int charMP = 10;
+    [SerializeField] int charStr = 2;
+    [SerializeField] int charDef = 2;
+    [SerializeField] int charInt = 2;
+    [SerializeField] int charSpd = 2;
+    [SerializeField] int charWil = 2;
+    [SerializeField] int charCon = 2;
+    [SerializeField] int charLck = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
