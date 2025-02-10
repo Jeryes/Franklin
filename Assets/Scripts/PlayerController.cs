d8b22f9542486a5797048c19b6aec12521f3e1c7
Jean <jean.r.escalante@gmail.com>
2025-02-10 08:38:32 -0500

    Attempt to update controls script (with errors)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent( typeof(CharacterController) )]
 public class PlayerController : MonoBehaviour
 {
     // Variables
+    private PlayerControls playerControls;
+    
+    private Vector2 moveDirection;
     public float speed = 5.0f;              // Movement speed
     public float rotationSpeed = 2.0f;      // Rotation speed
     public float gravity = -9.81f;          // Gravity acceleration
@@ -14,25 +18,48 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;                // To check if the player is on the ground
     private CharacterController controller; // A reference for the CharacterController component
     public GameObject playerBody;
+    public InputAction move;
+    public InputAction jump;
 
     void Awake()
     {
+        playerControls = new PlayerControls();
+        
         controller = GetComponent<CharacterController>();
         playerBody = GameObject.Find("Player/PlayerBody");
+        move = InputSystem.actions.FindAction("Move");
+        jump = InputSystem.actions.FindAction("Jump");
+    }
+
+    void OnEnable() {
+        playerControls.Enable();
+    }
+
+    void OnDisable() {
+        playerControls.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 move = playerControls.Gameplay.Move.ReadValue<Vector2>();
+        Debug.Log(move);
+        //playerControls.Gameplay.Jump.ReadValue<float>();
+        //if (playerControls.Gameplay.Jump.ReadValue<float>() == 1)
+        if (playerControls.Gameplay.Jump.triggered)
+            Debug.Log("Jump");
+
+
         // Ground check - make sure the character is on the ground
         isGrounded = controller.isGrounded;
 
         // Input handling
-        float horizontal = Input.GetAxis("Horizontal");  // Get input for left/right
-        float vertical = Input.GetAxis("Vertical");      // Get input for forward/backward
+        //float horizontal = Input.GetAxis("Horizontal");  // Get input for left/right
+        //float vertical = Input.GetAxis("Vertical");      // Get input for forward/backward
 
         // Combine into movement vector (X and Z axis)
-        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        //Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        moveDirection = move.ReadValue<Vector2>();
         
         // Normalize vector to ensure consistent speed in all directions
         if (moveDirection.magnitude > 1)
